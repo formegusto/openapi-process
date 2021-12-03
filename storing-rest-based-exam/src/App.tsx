a45c785dda44147a6a6c95fec7bf5d791b4ea04a
formegusto <formegusto@gmail.com>
2021-12-03 15:47:43 +0900

    exam ready okay

--- storing-rest-based-exam/src/App.tsx
+++ storing-rest-based-exam/src/App.tsx
@@ -1,19 +1,76 @@
+import axios from "axios";
 import React from "react";
 import "./styles/block.css";
 
-// 사용자의 마우스 움직임 감지
+// 사용자의 마우스 클릭 감지
+type Offset = {
+  x: number;
+  y: number;
+};
 function App() {
+  const [offset, setOffset] = React.useState<Offset>({
+    x: 0,
+    y: 0,
+  });
+  const refX = React.useRef(0);
+  const refY = React.useRef(0);
+  // const saveOffset = React.useCallback(
+  //   async (e: React.MouseEvent<HTMLDivElement>) => {
+  //     const elMMI = document.getElementById("mouse-movement-info");
+
+  //     if (elMMI) {
+  //       const { clientX: x, clientY: y } = e;
+  //       elMMI.classList.add("click");
+  //       const result = await axios.post(
+  //         "http://localhost:8080/mouse-movement",
+  //         {
+  //           x,
+  //           y,
+  //         }
+  //       );
+  //       console.log(result.data);
+  //     }
+  //   },
+  //   []
+  // );
+  const saveOffset = React.useCallback(async () => {
+    const elMMI = document.getElementById("mouse-movement-info");
+
+    if (elMMI) {
+      const result = await axios.post("http://localhost:8080/mouse-movement", {
+        ...offset,
+      });
+      console.log(result.data);
+    }
+  }, [offset]);
+
+  React.useEffect(() => {
+    saveOffset();
+  }, [saveOffset]);
+
+  React.useEffect(() => {
+    setInterval(async () => {
+      setOffset({
+        x: refX.current,
+        y: refY.current,
+      });
+    }, 5000);
+  }, []);
+
   const mouseMovementDetect = React.useCallback(
     (e: React.MouseEvent<HTMLDivElement>) => {
-      console.log(e.clientX);
-      console.log(e.clientY);
-
       const elMMI = document.getElementById("mouse-movement-info");
+
       if (elMMI) {
-        const { clientX: X, clientY: Y } = e;
-        elMMI.style.top = Y + "px";
-        elMMI.style.left = X + "px";
-        elMMI.textContent = `${X},${Y}`;
+        // elMMI.classList.remove("click");
+        const { clientX: x, clientY: y } = e;
+
+        refX.current = x;
+        refY.current = y;
+
+        elMMI.style.top = y + 10 + "px";
+        elMMI.style.left = x + 10 + "px";
+        elMMI.textContent = `${x},${y}`;
       }
     },
     []
@@ -24,6 +81,7 @@ function App() {
       <div
         id="mouse-movement-detect"
         onMouseMoveCapture={mouseMovementDetect}
+        // onMouseUp={saveOffset}
       />
       <div id="mouse-movement-info" />
     </>
--- storing-rest-based-server-exam/src/index.ts
+++ storing-rest-based-server-exam/src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import mongooseInit from "./models";
 import { MouseMovementInfo } from "./models/MouseMovement/types";
 import MouseMovementModel from "./models/MouseMovement";
+import cors from "cors";
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ const app = express();
   await mongooseInit();
 })();
 
+app.use(cors());
 app.use(express.json());
 
 app.post("/mouse-movement", async (req: Request, res: Response) => {
